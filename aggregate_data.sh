b8b841c83fd988171666a9a1964997c6888f24d3
Yujin Kim <37772661+randrover@users.noreply.github.com>
2025-02-27 11:02:06 +0900

    Update aggregate_data.sh

--- aggregate_data.sh
+++ aggregate_data.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 # Define the paths to your variables
 FILTER_VARIANT_PATH="/data2/deepLN/variants/kor_sfari_mssng.noncoding_DNV.20250226.tsv.gz"
 OUTPUT_PATH="/data2/deepLN/kor_sfari_mssng.enformer_sei_puffin.new_agg.v1.zarr"
+OUTPUT_PATH2="/data2/deepLN/kor_sfari_mssng.noncoding_raw.new_agg.v1.zarr"
 SAMPLE_LIST_PATH="/data2/deepLN/Korean_SFARI_MSSNG.14606samples.sample_list.20250224.txt"
 NC_CPUS=10
 
@@ -11,3 +14,10 @@ python sei_puffin_enformer_agg_filter.py \
   --output_path "$OUTPUT_PATH" \
   --sample_list_path "$SAMPLE_LIST_PATH" \
   --n_cpus "$NC_CPUS"
+
+# Step 2: Run noncoding_annot_gather.py
+echo "Running noncoding_annot_gather.py..."
+python noncoding_annot_gather.py \
+  --zarr_input "$OUTPUT_PATH" \
+  --zarr_output "$OUTPUT_PATH2" \
+  --filter_variant "$FILTER_VARIANT_PATH"
