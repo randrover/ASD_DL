48b643e754121df7e7659ee05a4acab0511eb8b8
Yujin Kim <37772661+randrover@users.noreply.github.com>
2025-06-17 15:34:39 +0100

    Delete sort_variant_for_cwas.sh

--- sort_variant_for_cwas.sh
+++
@@ -1,5 +0,0 @@
-grep -a "^#" Korean_SFARI_MSSNG_WGS_autosomal_DNV.14606samples.20250226.vcf > Korean_SFARI_MSSNG_WGS_autosomal_DNV.14606samples.sorted.20250226.vcf
-grep -v "^#" Korean_SFARI_MSSNG_WGS_autosomal_DNV.14606samples.20250226.vcf | sort -k1,1V -k2n >> Korean_SFARI_MSSNG_WGS_autosomal_DNV.14606samples.sorted.20250226.vcf
-
-bgzip -c Korean_SFARI_MSSNG_WGS_autosomal_DNV.14606samples.sorted.20250226.vcf > Korean_SFARI_MSSNG_WGS_autosomal_DNV.14606samples.sorted.20250226.vcf.gz
-tabix -p vcf Korean_SFARI_MSSNG_WGS_autosomal_DNV.14606samples.sorted.20250226.vcf.gz
