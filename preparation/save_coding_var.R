b969ae55d59368064fddfe361420855f30ac3715
Yujin Kim <37772661+randrover@users.noreply.github.com>
2025-06-24 20:51:34 +0100

    Update and rename save_coding_var.R to preparation/save_coding_var.R

--- preparation/save_coding_var.R
+++ preparation/save_coding_var.R
@@ -2,12 +2,12 @@ rm(list = ls())
 library(tidyverse)
 date = '20250227'
 
-dnv = data.table::fread('~/Dropbox/AI_NDD/Data/variants/Korean_SFARI_MSSNG.DNV_list.annotated.20250226.tsv.gz')
+dnv = data.table::fread('Korean_SFARI_MSSNG.DNV_list.annotated.20250226.tsv.gz')
 dnv = dnv %>%
   filter(is_coding==1)
 data.table::fwrite(dnv %>%
                      dplyr::select(variant, SAMPLE),
-                   paste('~/Dropbox/Noncoding_kor_ASD_WD/Tables/kor_sfari_mssng.coding_DNV', date, 'tsv.gz',
+                   paste('kor_sfari_mssng.coding_DNV', date, 'tsv.gz',
                          sep = '.'),
                    quote = F, row.names = F, col.names = T, sep = '\t',
                    compress="gzip")
