28cd084d958680af298e9ca6bb4438c68291b269
Yujin Kim <37772661+randrover@users.noreply.github.com>
2025-06-24 21:02:57 +0100

    Update and rename aggregate_data.sh to preparation/aggregate_data.sh

--- preparation/aggregate_data.sh
+++ preparation/aggregate_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define the paths to your variables
-FILTER_VARIANT_PATH="/data2/deepLN/variants/kor_sfari_mssng.noncoding_DNV.20250226.tsv.gz"
-OUTPUT_PATH="/data2/deepLN/kor_sfari_mssng.enformer_sei_puffin.new_agg.v1.zarr"
-OUTPUT_PATH2="/data2/deepLN/kor_sfari_mssng.noncoding_raw.new_agg.v1.zarr"
-SAMPLE_LIST_PATH="/data2/deepLN/Korean_SFARI_MSSNG.14606samples.sample_list.20250224.txt"
+FILTER_VARIANT_PATH="kor_sfari_mssng.noncoding_DNV.20250226.tsv.gz"
+OUTPUT_PATH="kor_sfari_mssng.enformer_sei_puffin.new_agg.v1.zarr"
+OUTPUT_PATH2="kor_sfari_mssng.noncoding_raw.new_agg.v1.zarr"
+SAMPLE_LIST_PATH="Korean_SFARI_MSSNG.14606samples.sample_list.20250224.txt"
 NC_CPUS=10
 
 # Step 1: Run sei_puffin_enformer_agg_filter.py
